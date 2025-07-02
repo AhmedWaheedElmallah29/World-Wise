9ab8a835664c2d8b4ef870ff6bf4df16952a8ccf
Elmallah <ahmedwaheedelmallah2@gmail.com>
2025-07-02 07:41:05 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
@@ -9,6 +9,7 @@ import CityList from "./components/CityList";
 import PageNotFound from "./pages/PageNotFound";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
@@ -35,10 +36,7 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="/" element={<Homepage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -48,6 +46,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
